dea4c1f7a39d45550c332a1291674653e8a87af2
Josh Shi <thehandsomepanther@gmail.com>
2017-01-06 14:07:10 -0600

    a better way of handling notifications (hopefully)

--- util/parseMessage.js
+++ util/parseMessage.js
@@ -1,19 +1,31 @@
 let _ = require('lodash')
 
-module.exports = function parseMessage(text, userList, channelList) {
+module.exports = function parseMessage(text, userList, channelList, format=true) {
   let userReg = /<@([^>\|]*)\|?([^>]*)>/g
   let channelReg = /<#([^>\|]*)\|?@?([^>]*)>/g
   let groupReg = /<!([^>\|]*)\|?@?([^>]*)>/g
+  let color = ''
+  if (format) {
+    color = 'red-fg'
+  }
 
   let match
+
   while (match = userReg.exec(text)) {
-    text = _.replace(text, match[0], `{red-fg}@${userList[match[1]]}{/red-fg}`)
+    text = _.replace(text, match[0], makeMessage(`@${userList[match[1]]}`, color))
   }
   while (match = groupReg.exec(text)) {
-    text = _.replace(text, match[0], `{red-fg}@${match[2].length?match[2]:match[1]}{/red-fg}`)
+    text = _.replace(text, match[0], makeMessage(`@${match[2].length?match[2]:match[1]}`, color))
   }
   while (match = channelReg.exec(text)) {
-    text = _.replace(text, match[0], `{red-fg}#${channelList[match[1]]}{/red-fg}`)
+    text = _.replace(text, match[0], makeMessage(`#${channelList[match[1]]}`, color))
   }
   return text
+}
+
+function makeMessage(message, color) {
+  if (color.length) {
+    return `{${color}}${message}{/${color}}`
+  }
+  return message
 }
