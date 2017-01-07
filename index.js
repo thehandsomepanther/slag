82c41fca4a74d456e2d2c016c70ce58ab92f363f
Josh Shi <thehandsomepanther@gmail.com>
2017-01-07 14:55:14 -0600

    display teams available and key shortcuts

--- index.js
+++ index.js
@@ -18,25 +18,25 @@ let {
   logHistory } = require('./util')
 
 let bot = slack.rtm.client()
-let tokens = getTokens()
+let tokenList = getTokens()
 let t = 0
-let token = tokens[t]
+let token = tokenList[t].token
+let teamSwitchKeys = 'asdfghjkl'
 bot.listen({token})
 
 let screen = blessed.screen({
   fullUnicode: true
 })
+assignScreenEvents()
+
 let grid = new contrib.grid({
   rows: 12, cols: 12, screen: screen
 })
 
-if (tokens.length > 1) {
-  screen.key(['C-t'], onTeamChange)
-}
-
-function onTeamChange(ch, key) {
+function onTeamChange(i) {
+  t = i
   bot.close()
-  token = tokens[(++t) % tokens.length]
+  token = tokenList[i % tokenList.length].token
   bot.listen({token})
   screen.destroy()
   screen = blessed.screen({
@@ -45,7 +45,7 @@ function onTeamChange(ch, key) {
   grid = new contrib.grid({
     rows: 12, cols: 12, screen: screen
   })
-  screen.key(['C-t'], onTeamChange)
+  assignScreenEvents()
   getTeamData(token, (teamData) => {
     prepareScreen(teamData)
   })
@@ -54,6 +54,24 @@ function onTeamChange(ch, key) {
 getTeamData(token, (teamData) => {
   prepareScreen(teamData)
 })
+
+function assignScreenEvents() {
+  if (tokenList.length > 1) {
+    screen.key(['C-t'], (ch, key) => {
+      onTeamChange(++t)
+    })
+  }
+
+  for (let i = 0; i < Math.min(teamSwitchKeys.length, tokenList.length); i++) {
+    screen.key([`C-${teamSwitchKeys[i]}`], (ch, key) => {
+      onTeamChange(teamSwitchKeys.indexOf(key.name))
+    })
+  }
+
+  screen.key(['escape', 'C-c'], (ch, key) => {
+    return process.exit(0)
+  })
+}
 
 function prepareScreen(teamData) {
   _.extend(teamData, {
@@ -83,9 +101,12 @@ function prepareScreen(teamData) {
     sendMessage(teamData, message)
   })
 
-  let tree = grid.set(0, 0, 12, 4, contrib.tree, {
+  let tree = grid.set(0, 0, 9, 4, contrib.tree, {
     label: `${teamData.currentTeam}`,
     tags: true,
+    template: {
+      lines: true
+    }
   })
   tree.style.border = border
 
@@ -104,6 +125,23 @@ function prepareScreen(teamData) {
 
   tree.setData(teamData.channelTree)
 
+  let teamDisplay = grid.set(9, 0, 3, 4, contrib.tree, {
+    label: `Your Teams`,
+    tags: true,
+    template: {
+      lines: true
+    }
+  })
+  teamDisplay.setData({
+    extended: true,
+    children: makeTeamDisplayChildren(tokenList)
+  })
+  teamDisplay.style.border = border
+
+  teamDisplay.on('select', (node) => {
+    onTeamChange(node.index)
+  })
+
   let now = parseFloat(timestamp.now())
   bot.message((message) => {
     if (parseFloat(message.ts) > now) {
@@ -111,7 +149,7 @@ function prepareScreen(teamData) {
     }
   })
 
-  let foci = [tree, input]
+  let foci = [tree, input, teamDisplay]
   var currentFocus = 0
 
   foci[currentFocus].focus()
@@ -125,10 +163,6 @@ function prepareScreen(teamData) {
     screen.render()
   })
 
-  screen.key(['escape', 'C-c'], (ch, key) => {
-    return process.exit(0)
-  })
-
   screen.on('resize', () => {
     init(teamData, log)
   })
@@ -139,4 +173,13 @@ function prepareScreen(teamData) {
 function init(teamData, log) {
   log.clearItems()
   logHistory(teamData, log)
+}
+
+function makeTeamDisplayChildren(tokenList) {
+  let children = {}
+  for (let i = 0; i < tokenList.length; i++) {
+    let item = tokenList[i]
+    children[`${item.team} {red-fg}(⌃${teamSwitchKeys[i]}){/red-fg}`] = _.extend(item, {index: i})
+  }
+  return children
 }
