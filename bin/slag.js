d43ae31741a75dd6c9cfbf8265c19c16fff99590
brandonfujii <brandonfujii2018@u.northwestern.edu>
2017-01-25 19:22:57 -0600

    print banner on slag

--- bin/slag.js
+++ bin/slag.js
@@ -4,6 +4,17 @@ let yargs = require('yargs')
 let fs = require('fs-extra')
 let path = require('path')
 
+const bannerPath = path.join(__dirname, '../assets/banner.txt')
+
+try {
+  const banner = fs.readFileSync(bannerPath, { encoding: 'utf-8' })
+  console.log(banner)
+} catch(err) {
+  if (err.code == 'ENOENT') 
+    console.log('Welcome to Slag Client')
+}
+
+
 let ARGS = yargs.argv
 
 if (ARGS.h || ARGS.help) {
