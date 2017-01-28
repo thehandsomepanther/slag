f909e709422de30051273082100622ec9f80b036
Josh Shi <thehandsomepanther@gmail.com>
2017-01-27 19:39:16 -0600

    add --add-token flag

--- util/getTokens.js
+++ util/getTokens.js
@@ -3,20 +3,35 @@ let path = require('path')
 let errors = require('../errors')
 
 module.exports = function getTokens() {
-  try {
-    if (process.env['SLAG_TOKENS']) {
-      fs.copySync(process.env['SLAG_TOKENS'], path.join(__dirname, '../tokens.json'))
-    }
+  const TOKENSPATH = path.join(__dirname, '../tokens.json')
 
-    const tokens = fs.readFileSync(path.join(__dirname, '../tokens.json'))
-    return JSON.parse(tokens)
-
-  } catch(exception) {
-    if (exception.code === 'ENOENT') {
-      console.log('Tokens.json file missing!\n', new errors.ConfigurationError('Use `slag --help` to learn how to set a tokens file.', null))
-    } else {
-      console.log('Tokens.json file incorrect!\n', new errors.ConfigurationError('There\'s something wrong with your tokens.json configuration file\nMake sure it\'s in the format specified here: https://github.com/thehandsomepanther/slag/blob/master/README.md', null))
-    }
-    process.exit(1)
+  if (process.env['SLAG_TOKENS_PATH']) {
+    fs.copySync(process.env['SLAG_TOKENS_PATH'], TOKENSPATH)
   }
+
+  let tokens = []
+  try {
+    tokens = JSON.parse(fs.readFileSync(TOKENSPATH))
+  } catch(e) {
+    if (!process.env['SLAG_TOKEN']) {
+      if (exception.code === 'ENOENT') {
+        console.log('Tokens.json file missing!\n', new errors.ConfigurationError('Use `slag --help` to learn how to set a tokens file.', null))
+      } else {
+        console.log('Tokens.json file incorrect!\n', new errors.ConfigurationError('There\'s something wrong with your tokens.json configuration file\nMake sure it\'s in the format specified here: https://github.com/thehandsomepanther/slag/blob/master/README.md', null))
+      }
+      process.exit(1)
+    }
+  }
+
+  if (process.env['SLAG_TOKEN']) {
+    tokens.push({
+      team: process.env['SLAG_TOKEN_TEAM'],
+      token: process.env['SLAG_TOKEN']
+    })
+    fs.writeJsonSync(TOKENSPATH, tokens)
+    console.log(`Added token to team ${process.env['SLAG_TOKEN_TEAM']}`)
+    process.exit(0)
+  }
+
+  return tokens
 }
