dcadf1d15e1a874204764caee5362c50666c5108
Josh Shi <thehandsomepanther@gmail.com>
2017-01-29 14:36:43 -0600

    improve add token, add list-tokens

--- util/getTokens.js
+++ util/getTokens.js
@@ -24,12 +24,26 @@ module.exports = function getTokens() {
   }
 
   if (process.env['SLAG_TOKEN']) {
+    for (t of tokens) {
+      if (t.token == process.env['SLAG_TOKEN']) {
+        console.log(`This token has already been registered!`)
+        process.exit(0)
+      }
+    }
+
     tokens.push({
       team: process.env['SLAG_TOKEN_TEAM'],
       token: process.env['SLAG_TOKEN']
     })
     fs.writeJsonSync(TOKENSPATH, tokens)
     console.log(`Added token to team ${process.env['SLAG_TOKEN_TEAM']}`)
+    process.exit(0)
+  }
+
+  if (process.env['LIST_TOKENS']) {
+    for (token of tokens) {
+      console.log(`${token.team}: \t${token.token}`)
+    }
     process.exit(0)
   }
 
